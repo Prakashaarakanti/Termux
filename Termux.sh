a1e436f9153404ab4549f98b5925139591a87806
Aarakanti Prakash <35517081+Prakashaarakanti@users.noreply.github.com>
2020-05-12 18:12:32 +0530

    Update Termux.sh

--- Termux.sh
+++ Termux.sh
@@ -1,3 +1,8 @@
+pkg insatll cowsay
+pkg insatll figlet
+pkg insatll toilet
+
+pkg install cowsay
 sl 
 sleep 3
 
@@ -5,8 +10,10 @@ cowsay yerripooka
 
 
 sleep 3
-figlet Good evening BAVA
+figlet Good evening
+
 sleep 5
+toilet I LOVE BAA
 cacafire
 sleep 2
 exit
